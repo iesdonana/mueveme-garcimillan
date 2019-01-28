a3f5f38fd934f487888d9a92405ebd8788188b6c
Pepe Millan <jose.millan@iesdonana.org>
2019-01-28 17:38:13 +0100

    Create table usuarios (hecho por ricardo)

--- db/mueveme.sql
+++ db/mueveme.sql
@@ -2,3 +2,9 @@
 -- Archivo de base de datos --
 ------------------------------
 
+CREATE TABLE usuarios
+(
+    id            BIGSERIAL          PRIMARY KEY
+  , nombre        VARCHAR(32)        NOT NULL UNIQUE
+  , password      VARCHAR(60)        NOT NULL
+);
