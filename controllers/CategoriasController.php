a9fa2f779b29bb8e4174389a69ff2d51011b8437
Pepe Millan <jose.millan@iesdonana.org>
2019-02-15 21:15:44 +0100

    añado las librerias de dropbox

--- controllers/CategoriasController.php
+++ controllers/CategoriasController.php
@@ -2,12 +2,12 @@
 
 namespace app\controllers;
 
-use Yii;
 use app\models\Categorias;
 use app\models\CategoriasSearch;
+use Yii;
+use yii\filters\VerbFilter;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
 
 /**
  * CategoriasController implements the CRUD actions for Categorias model.
@@ -46,7 +46,7 @@ class CategoriasController extends Controller
 
     /**
      * Displays a single Categorias model.
-     * @param integer $id
+     * @param int $id
      * @return mixed
      * @throws NotFoundHttpException if the model cannot be found
      */
@@ -78,7 +78,7 @@ class CategoriasController extends Controller
     /**
      * Updates an existing Categorias model.
      * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
+     * @param int $id
      * @return mixed
      * @throws NotFoundHttpException if the model cannot be found
      */
@@ -98,7 +98,7 @@ class CategoriasController extends Controller
     /**
      * Deletes an existing Categorias model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
+     * @param int $id
      * @return mixed
      * @throws NotFoundHttpException if the model cannot be found
      */
@@ -112,7 +112,7 @@ class CategoriasController extends Controller
     /**
      * Finds the Categorias model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
+     * @param int $id
      * @return Categorias the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
@@ -124,4 +124,9 @@ class CategoriasController extends Controller
 
         throw new NotFoundHttpException('The requested page does not exist.');
     }
+
+    // public function actionList()
+    // {
+    //     return Categorias::find()->limit(20)->all();
+    // }
 }
